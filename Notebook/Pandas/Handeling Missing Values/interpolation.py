21f8a24be854126d4a6b319302f196980a2ea747
raisuuuu2023 <raisa22025000@gmail.com>
2026-01-28 23:55:17 +0600

    ...

---
+++ Notebook/Pandas/Handeling Missing Values/interpolation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data={
+    "Time":[1,2,3,4,5],
+    "Value":[10,None,30,None,50]
+}
+df=pd.DataFrame(data)
+print('Before Interpolation')
+print(df)
+df['Value']=df['Value'].interpolate(method="linear")
+print('After interpolation')
+print(df)
---
+++ Notebook/Pandas/adding.py
@@ -0,0 +1,13 @@
+import pandas as pd
+data={
+    "Name":['Ram','Shyan','Ghanshyam','Dhanshyam','Aditi','Jagdish','Raj','Simran'],
+    "Age":[28,34,22,30,29,40,25,32],
+    "Salary":[50000,60000,45000,52000,49000,70000,48000,58000],
+    "Performance_Score":[85,90,78,92,88,95,80,89]
+}
+df=pd.DataFrame(data)
+print(df)
+df['Bonus']=df['Salary']*0.1
+print(df)
+df.insert(0,'Employee ID',[10,20,30,40,50,60,70,80])
+print(df)
